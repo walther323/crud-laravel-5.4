e9b157e05617abf229ff576d5668720724b295a3
walther <walter323@gmail.com>
2017-06-19 15:45:48 -0500

    Listar y Paginar elementos en LARAVEL 5.4

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,7 +2,36 @@
 
 @section('content')
 	<div class="col-sm-8">
-		Listado de productos
+		<h2>
+			Listado de Productos
+			<a href="{{ route('products.create')}}" class="btn btn-primary pull-right">Nuevo</a>
+		</h2>
+
+		<table class="table table-hover table-striped">
+			<thead>
+				<tr>
+					<th width="20px">ID</th>
+					<th>Nombre del producto</th>
+					<th colspan="3">&nbsp;</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				@foreach($products as $product)
+					<tr>
+						<td>{{ $product->id }}</td>
+						<td>
+							<strong>{{ $product->name }}</strong>
+							{{ $product->short }}
+						</td>
+						<td>Ver</td>
+						<td>Editar</td>
+						<td>Borrar</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+		{!! $products->render() !!}
 	</div>
 
 	<div class="col-sm-4">
