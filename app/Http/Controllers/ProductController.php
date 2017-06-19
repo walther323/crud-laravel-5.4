2de61da4122422c8a95ce49a1e2f84ca1a9057cf
walther <walter323@gmail.com>
2017-06-19 17:46:44 -0500

    Letra C y U del CRUD

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,42 +10,58 @@ class ProductController extends Controller
 {
     public function index()
     {
-    	$products = Product::orderBy('id','DESC')->paginate();
-    	return view('products.index', compact('products'));
+        $products = Product::orderBy('id', 'DESC')->paginate();
+        return view('products.index', compact('products'));
     }
 
     public function create()
     {
-    	return view('products.create');
+        return view('products.create');
     }
 
     public function store(ProductRequest $request)
     {
-    	return 'producto guardado';
+    	$product = new Product;
+
+        $product->name  = $request->name;
+        $product->short = $request->short;
+        $product->body  = $request->body;
+
+        $product->save();
+
+        return redirect()->route('products.index')->with('info', 'El Producto fue guardado');
     }
 
     public function edit($id)
     {
-    	$product = Product::find($id);
-    	return view('products.edit', compact('product'));
+        $product = Product::find($id);
+        return view('products.edit', compact('product'));
     }
 
     public function update(ProductRequest $request, $id)
     {
-    	return 'producto actualizado: '.$id;
+        $product = Product::find($id);
+
+        $product->name  = $request->name;
+        $product->short = $request->short;
+        $product->body  = $request->body;
+
+        $product->save();
+
+        return redirect()->route('products.index')->with('info', 'El Producto '.$id.' fue actualizado');
     }
 
     public function show($id)
     {
-    	$product = Product::find($id);
-    	return view('products.show', compact('product'));
+        $product = Product::find($id);
+        return view('products.show', compact('product'));
     }
 
     public function destroy($id)
     {
-    	$product = Product::find($id);
-    	$product->delete();
+        $product = Product::find($id);
+        $product->delete();
 
-    	return back()->with('info','El producto fue Eliminado');
+        return back()->with('info', 'El producto fue Eliminado');
     }
 }
